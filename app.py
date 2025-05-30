8543ecbe1c5e0c067cdbcde06bbb0f5e9ec6da82
Jiwon <jiwonkim1207@gmail.com>
2025-05-30 13:48:44 +0900

    Separated calendar event and restaurant endpoints

--- app.py
+++ app.py
@@ -5,34 +5,14 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/mealtime-labels', methods=['POST'])
-def get_mealtime_labels():
-    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    review_json = request.get_json(force=True)
-    review = review_json["review"]
-    
-    mealtime_labels = ["breakfast", "lunch", "dinner", "late-night"]
-
-    res = classifier(review, candidate_labels=mealtime_labels, multi_label=True)
-
-    mealtime_keywords = ""
-
-    for label, score in zip(res["labels"], res["scores"]):
-        print(f"{label}: {score:.2f}")
-        if score > 0.30:
-            mealtime_keywords += label
-            mealtime_keywords += ", "
-    
-    return jsonify(mealtime_keywords)
-
-@app.route('/semantic-labels', methods=['POST'])
+@app.route('/restaurant-labels', methods=['POST'])
 def get_semantic_labels():
     classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
     review_json = request.get_json(force=True)
-    review = review_json["review"]
+    review = review_json["text"]
     
-    semantic_labels = ["romantic", "hangout", "business casual", "family", "celebration", "quick bite"]
+    semantic_labels = ["comfort food", "quiet", "fast", "romantic", "gourmet", "budget", "casual", "relaxed", "dessert", "group", "loud", "lively"]
 
     res = classifier(review, candidate_labels=semantic_labels, multi_label=True)
 
@@ -40,11 +20,29 @@ def get_semantic_labels():
 
     for label, score in zip(res["labels"], res["scores"]):
         print(f"{label}: {score:.2f}")
-        if score > 0.30:
+        if score > 0.75:
             semantic_keywords += label
             semantic_keywords += ", "
     
     return jsonify(semantic_keywords)
+
+@app.route('/mood-labels', methods=['POST'])
+def get_semenatic():
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+    review_json = request.get_json(force=True)
+    review = review_json["text"]
+    
+    semantic_labels = ["stressed", "romantic", "busy", "celebration", "relaxed"]
+
+    res = classifier(review, candidate_labels=semantic_labels, multi_label=True)
+
+    max_index = res['scores'].index(max(res['scores']))
+
+    # Get the top label
+    top_mood = res['labels'][max_index]
+    
+    return jsonify(top_mood)
 
 if __name__ == '__main__':
    app.run(port=5001)
